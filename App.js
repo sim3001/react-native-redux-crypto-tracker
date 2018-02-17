cbe5f06e0b634a5461ccd84cfdf6d7ea2a041219
Simranjit Singh <simranjit.boyal@gmail.com>
2018-02-16 17:16:17 -0700

    Added Container,Store,Reducers

--- App.js
+++ App.js
@@ -1,22 +1,19 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Header } from './src/components';
+import React, {Component} from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
 
-export default class App extends React.Component {
+import Store from './src/Store';
+import { Header, CryptoContainer } from './src/components';
+
+export default class App extends Component {
   render() {
     return (
-      <View>
-        <Header />
-      </View>
+      <Provider store={Store}>
+        <View>
+          <Header />
+          <CryptoContainer />
+        </View>
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
